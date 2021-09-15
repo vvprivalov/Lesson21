8816c4276ea7a3da261e87fb9e02bc8b85055289
vv.privalov <vv.privalov@yandex.ru>
2021-09-15 22:52:18 +0300

    Закончил 21-й урок(организация гонки). Все-таки есть нюансы, с малым процентом вероятности неправильно определяет победителя.

--- src/main/java/RaceCar/MainClass.java
+++ src/main/java/RaceCar/MainClass.java
@@ -19,8 +19,8 @@ public class MainClass {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), barrier);
         }
 
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+        for (Car car : cars) {
+            new Thread(car).start();
         }
     }
 }
